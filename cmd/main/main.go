821fdd5d2d0e9183c5ef5ab2cb387d300b5ea335
aron-helu <aronmilahela@gmail.com>
2023-07-05 10:28:21 +0300

    main file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,19 @@
 package main
 
 import(
-	"fmt"
+	
+	"log"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/aron-helu/Book_Management-Go/pkg/routes"
 )
 
 func main() {
-	fmt.Println("Hello")
+	r:= httprouter.New()
+
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
