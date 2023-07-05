9deb16e109207c4bcfe77c3c8d599a5b274147fe
aron-helu <aronmilahela@gmail.com>
2023-07-05 10:26:42 +0300

    book Models package

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,2 +1,46 @@
 package models
 
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/aron-helu/Book_Management-Go/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publisher string `json:"publisher"`
+
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var Book Book
+	db.Where("ID=?", Id).Delete(Book)
+	return Book
+}
